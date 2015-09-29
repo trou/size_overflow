82194b1182056e118e160faf653590639191b1d8
Emese Revfy <re.emese@gmail.com>
2015-09-29 21:07:43 +0200

    release

--- size_overflow_plugin.c
+++ size_overflow_plugin.c
@@ -29,7 +29,7 @@ tree size_overflow_type_DI;
 tree size_overflow_type_TI;
 
 static struct plugin_info size_overflow_plugin_info = {
-	.version	= "20150926",
+	.version	= "20150929",
 	.help		= "no-size-overflow\tturn off size overflow checking\n",
 };
 
